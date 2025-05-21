4ae78dc9da012f0b10fd6dd55ff15e94c23987db
marie <borisova.marie1@gmail.com>
2025-05-21 12:58:28 +0200

    modified language files Kligon and Hungarian, added certificate images

--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -1,4 +1,7 @@
 import {useTranslation} from 'react-i18next';
+import jsCert1 from '../../public/js_1_cert.webp';
+import jsCert2 from '../../public/js_2_cert.webp';
+import linuxCert from '../../public/linux_cert.webp';
 
 
 export default function Certificates(){
@@ -9,23 +12,10 @@ export default function Certificates(){
         <>
             <h2>{t('certs.title')}</h2>
             <div id="certificates" className="mt-5">
-                <div className="fs-4 border border-dark border-4">
-                    <h4><strong> JavaScript Essentials 1 </strong> </h4>
-                    <p> Cisco Networking Academy program </p>
-                    <p> Datum dokončení: 20.01.2025 </p>
-                </div>
-                <div className="fs-4 border border-dark border-4">
-                    <h4> <strong> JavaScript Essentials 2 </strong> </h4>
-                    <p> Cisco Networking Academy program </p>
-                    <p> Datum dokončení: 03.03.2025 </p>
-                </div>
-                <div className="fs-4 border border-dark border-4">
-                    <h4> <strong> Linux Essentials </strong> </h4>
-                    <p> Cisco Networking Academy program </p>
-                    <p> Datum dokončení: 24.03.2025 </p>
-                </div>
 
-
+                <img src={jsCert1} width={500}/>
+                <img src={jsCert2} width={500}/>
+                <img src={linuxCert} width={500}/>
 
             </div>
 
